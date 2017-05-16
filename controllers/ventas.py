9b515ef67d3409f4479d282d2687e029cd2b052c
Lucas Montiel <lucas.empleo1@gmail.com>
2017-05-16 18:56:14 -0300

    carrito

--- controllers/ventas.py
+++ controllers/ventas.py
@@ -4,4 +4,9 @@ def VentasLocal():
     return dict()
 ########################################################
 def VentasOnline():
-    return dict()
+    "pagina de inicio del catalogo"
+    return dict ()
+
+def inicio():
+    "pagina de inicio del catalogo"
+    return dict(message="funcion  inicio")
