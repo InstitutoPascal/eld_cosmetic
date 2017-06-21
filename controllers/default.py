39d59b9ba814da4a251e1c04071559941994736b
Lucas Montiel <lucas.empleo1@gmail.com>
2017-06-21 16:29:43 -0300

    modificando vista catalogo

--- controllers/default.py
+++ controllers/default.py
@@ -20,6 +20,17 @@ def index():
     response.flash = T("Bienvenido a ELD Cosmetic")
     return dict(message=T('Sistema ELD_COSMETIC'))
 
+def index2():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simply replace the two lines below with:
+    return auth.wiki()
+    """
+    response.flash = T("Gracias por iniciar sesión!")
+    return dict(message=T('Sistema ELD Cosmetic'))
+
 def user():
     """
     exposes:
