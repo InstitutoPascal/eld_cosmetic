633ff662d7cc63b99014b229b67c8e8a024dce85
Enrique Delgado <technologylafe@gmail.com>
2017-09-02 13:03:53 -0300

    subiendo vistas y controladores nuevos

---
+++ controllers/ventaslocal.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# intente algo como
+def index(): return dict(message="hello from ventaslocal.py")
+
+def VentasLocal():
+    # definir los campos a obtener desde la base de datos:
+    campos = db.cliente.id_cliente, db.cliente.nombre, db.cliente.codigo_cliente
+    # definir la condición que deben cumplir los registros:
+    criterio = db.cliente.id_cliente>0
+    # ejecutar la consulta:
+    lista_clientes = db(criterio).select(*campos)
+    # revisar si la consulta devolvio registros:
+    if not lista_clientes:
+        mensaje = "No ha cargado clientes"
+    else:
+        mensaje = "Seleccione un cliente"
+    #redirije los valores al HTML
+    return dict(message=mensaje, lista_clientes=lista_clientes,)
+
+
+def VentasLocalTarjeta():
+    return dict()
+
+def Borrar_Item():
+    return dict()
+
+def CancelarVenta():
+    return dict()
+
+def VentasLocalVistaprevia():
+    return dict()
+
+def VentasMediosPago():
+    return dict()
+
+def VentasLocalCarga():
+    return dict()
+
+
+def VentaLocalReporte():
+    grid = SQLFORM.grid(db.productos)
+    return {"grilla": grid}
